6591f10bb88fd0026f0b57f9516e98ca9eef34f1
xiang-yu <xiang.yu.li.phy@gmail.com>
2026-09-10 21:07:44 -0700

    penaltyDE_2d.pdf

--- penaltyDE_2d.py
+++ penaltyDE_2d.py
@@ -9,21 +9,42 @@ class PenaltyProj:
         convenience class for projection operator to provide time evolution
     '''
     def __init__(self, operator, kind):
-        self.operator = operator
-        self.kind = kind
-        assert (self.kind.lower() == 'value' or
-                self.kind.lower() == 'deriv')
-        if self.kind.lower() == 'deriv':
-            self.operator = self.operator.tocsc()
+        self.kind = kind.lower()
+        if self.kind not in {'value', 'deriv'}:
+            raise ValueError("kind must be either 'value' or 'deriv'")
+
+        self.operator = sparse.csc_matrix(operator, dtype=complex)
+        if self.operator.shape[0] != self.operator.shape[1]:
+            raise ValueError('projection operator must be square')
+        ident = sparse.identity(
+            self.operator.shape[0], dtype=complex, format='csc'
+        )
+        self.complement = (ident - self.operator).tocsc()
 
     def apply_exp_fn(self, lam):
-        if self.kind.lower() == 'value':
-            out_fn = lambda t: diags(np.exp(1.j*lam*t*self.operator.diagonal()),0)
-        elif self.kind.lower() == 'deriv':
-            # I was lazy here but this could also be
-            # "classically fast-forwarded"
-            out_fn = lambda t: sparse.linalg.expm(1.j*lam*t*self.operator)
-        return out_fn
+        '''Return ``exp(i * lam * t * P)`` using projector fast-forwarding.
+
+        Since ``P`` is an orthogonal projector, its exponential is exactly
+        ``(I - P) + exp(i * lam * t) P``.  Its construction therefore costs
+        only O(nnz(P)) and is independent of the magnitude of ``lam * t``.
+        '''
+        return lambda t: (
+            self.complement + np.exp(1.j * lam * t) * self.operator
+        )
+
+    def apply_exp_to(self, values, lam, t):
+        '''Apply ``exp(i * lam * t * P)`` without constructing a matrix.'''
+        values = np.asarray(values)
+        phase = np.exp(1.j * lam * t)
+        return values + (phase - 1.) * (self.operator @ values)
+
+    def project_feasible(self, values):
+        '''Project values onto the constraint-satisfying kernel of ``P``.'''
+        return self.complement @ np.asarray(values)
+
+    def constraint_residual(self, values):
+        '''Return the component that violates the encoded constraint.'''
+        return self.operator @ np.asarray(values)
 """ ========================================== """
 class PenaltyDE_2d:
     '''
@@ -36,9 +57,8 @@ class PenaltyDE_2d:
         self.T = T
         self.lam = lam
         self.x, self.y, self.dx = self._create_xy()
-
-        self.eqtype = eqtype
-        self.bctype = bctype
+        self.eqtype = eqtype.lower()
+        self.bctype = bctype.lower()
         self.coeff = coeff
         self._set_system_operator(coeff=coeff)
 
@@ -46,13 +66,14 @@ class PenaltyDE_2d:
         self.bndry_points = None # , bndry_vals = None, None
 
         self.projc = None
-        self._set_projector(bctype, **kwargs)
-
-        if not eqtype.lower() == 'wave':
+        self.spatial_projc = None
+        self.spatial_bndry_points = None
+        self._set_projector(self.bctype, **kwargs)
+        if not self.eqtype == 'wave':
             self.RHS         = np.zeros((self.N*self.N), dtype=complex)
             self.bcvals      = np.zeros((self.N*self.N), dtype=complex)
             self.L_at_bcvals = np.zeros((self.N*self.N), dtype=complex)
-        elif eqtype.lower() == 'wave':
+        elif self.eqtype == 'wave':
             self.RHS         = np.zeros((2*self.N*self.N), dtype=complex)
             self.bcvals      = np.zeros((2*self.N*self.N), dtype=complex)
             self.L_at_bcvals = np.zeros((2*self.N*self.N), dtype=complex)
@@ -63,7 +84,6 @@ class PenaltyDE_2d:
         grid_1d = np.linspace(0, 1, self.N, endpoint=True)
         x, y = np.meshgrid(grid_1d, grid_1d)
         dx  = grid_1d[1] - grid_1d[0]
-
         return x, y, dx
     ''' -------------------------------------------------- '''
     def wall_bdry_projection2d(self, kind='lrtb'):
@@ -81,37 +101,20 @@ class PenaltyDE_2d:
         proj =  projOp(self.N*self.N, slit_indices(self.x, self.y))
         return proj, proj.diagonal().nonzero()
     ''' -------------------------------------------------- '''
-    def wall_deriv_projection2d(self):
-        ''' construct derivative projection based on first-order finite difference stencil '''
-        def clean(wall):
-            wall = np.delete(wall, wall.argmax())
-            wall = np.delete(wall, wall.argmin())
-            return wall
-
-        ''' geometry '''
-        wall_top = clean(wall_indices(self.x, self.y, L=0., R=1., kind='t'))
-        wall_bot = clean(wall_indices(self.x, self.y, L=0., R=1., kind='b'))
-        # double-clean two of the sides to avoid doubly applying it in the corner points
-        wall_lef = clean(clean(wall_indices(self.x, self.y, L=0., R=1., kind='l')))
-        wall_rig = clean(clean(wall_indices(self.x, self.y, L=0., R=1., kind='r')))
-
-        ''' swap part '''
-        swap =  sparse.coo_matrix((np.ones_like(wall_top), (wall_top, wall_top+self.N)), shape=(self.N**2,self.N**2))
-        swap += sparse.coo_matrix((np.ones_like(wall_bot), (wall_bot, wall_bot-self.N)), shape=(self.N**2,self.N**2))
-        swap += sparse.coo_matrix((np.ones_like(wall_lef), (wall_lef, wall_lef+1))     , shape=(self.N**2,self.N**2))
-        swap += sparse.coo_matrix((np.ones_like(wall_rig), (wall_rig, wall_rig-1))     , shape=(self.N**2,self.N**2))
-
-        bndry_pts = np.array([*zip(*swap.nonzero())],dtype=int)
-        proj = swap + swap.T
-
-        return proj, bndry_pts
+    def wall_deriv_projection2d(self, kind='lrtb'):
+        '''Construct the zero-Neumann projector for selected box walls.'''
+        proj = wall_deriv_projection2d(
+            self.N, self.x, self.y, kind=kind
+        )
+        return proj, proj.diagonal().nonzero()
     ''' -------------------------------------------------- '''
     def _set_projector(self, bctype, **kwargs):
         '''
             set projection operation depending on boundary condition
             todo for future: mixed conditions with mixed/non-overlapping projections
         '''
-        # If heat equation, select one of 'wall', 'slit', 'circle'
+        bctype = bctype.lower()
+        proj_kind = 'value'
         if bctype == 'wall':
             if 'kind' in kwargs.keys():
                 projc, bndry_points = self.wall_bdry_projection2d(kwargs['kind'])
@@ -121,26 +124,31 @@ class PenaltyDE_2d:
             projc, bndry_points = self.circle_bdry_projection2d()
         elif bctype == 'slit':
             projc, bndry_points = self.slit_bdry_projection2d()
-        elif bctype == 'wall_deriv':
-            projc, bndry_points = self.wall_deriv_projection2d()
+        elif bctype in {'neumann', 'wall_neumann', 'wall_deriv'}:
+            projc, bndry_points = self.wall_deriv_projection2d(
+                kwargs.get('kind', 'lrtb')
+            )
+            proj_kind = 'deriv'
         else:
-            raise Exception("Boundary type {bb} not implemented.".format(bb=bctype))
+            raise ValueError("Boundary type {bb} not implemented.".format(bb=bctype))
 
-        if not self.eqtype.lower() == 'wave':
-            self.bndry_points = bndry_points
-            if not 'deriv' in bctype.lower():
-                self.projc = PenaltyProj(operator=projc, kind='value')
-            else:
-                self.projc = PenaltyProj(operator=projc, kind='deriv')
-        # If wave equation, need to account for change of variables structure
-        # So far also haven't implemented derivative constraints here
-        elif self.eqtype.lower() == 'wave':
-            assert 'deriv' not in bctype.lower()
-            wave_projc = sparse.block_array([[projc, None], [None, projc]])
-            wave_bndry_points = wave_projc.diagonal().nonzero()
-            self.bndry_points = wave_bndry_points
-            # np.array([*zip(*swap.nonzero())],dtype=int)
-            self.projc = PenaltyProj(operator=wave_projc, kind='value')
+        self.spatial_projc = sparse.csc_matrix(projc, dtype=complex)
+        self.spatial_bndry_points = bndry_points
+        if self.eqtype != 'wave':
+            self.bndry_points = self.spatial_bndry_points
+            self.projc = PenaltyProj(
+                operator=self.spatial_projc, kind=proj_kind
+            )
+        else:
+            # If d_n u = 0 for all t, then d_n w = d_t(d_n u) = 0 as
+            # well.  Apply the same spatial constraint to u and w.
+            wave_projc = sparse.block_diag(
+                (self.spatial_projc, self.spatial_projc), format='csc'
+            )
+            self.bndry_points = wave_projc.diagonal().nonzero()
+            self.projc = PenaltyProj(
+                operator=wave_projc, kind=proj_kind
+            )
     ''' -------------------------------------------------- '''
     def _set_system_operator(self, coeff):
         ''' set system matrix '''
@@ -149,7 +157,7 @@ class PenaltyDE_2d:
         elif self.eqtype.lower() == 'wave':
             self.operator = wave_operator2d(self.N, speed_of_sound=coeff, dx=self.dx)
         else:
-            raise Exception("Equation type {ee} not implemented.".format(bb=eqtype))
+            raise ValueError("Equation type {ee} not implemented.".format(ee=self.eqtype))
     ''' -------------------------------------------------- '''
     def construct_initial_state_centered(self, width, height):
         ''' construct initial state, Gaussian in center of box '''
@@ -157,15 +165,11 @@ class PenaltyDE_2d:
         gaussian = height * np.exp( -1.*(np.square(self.x-1/2)+np.square(self.y-1/2)) / width )
         self.x0 = np.array(gaussian, dtype=complex).flatten()
         # outpoints = circle_points(self.x, self.y, radius=0.4, kind='outside')
-        # Make sure that BC is satisfied -- given homogenization, this is always zero for Dirichlet
-        if self.projc.kind == 'value':
-            self.x0[self.bndry_points] = 0.
-        elif self.projc.kind == 'deriv':
-            print("Warning: Depending on the projection used the initial condition might have to be adapted.")
-            self.x0[self.bndry_points] = 0.
+        # Make sure the homogeneous value or derivative constraint is met.
+        self.x0 = self.projc.project_feasible(self.x0)
     ''' -------------------------------------------------- '''
     def construct_initial_state_wave(self):
-        ''' construct initial state, slit geometry '''
+        '''Construct the compactly supported initial wave state.'''
         assert self.eqtype.lower() == 'wave'
         outpoints = circle_points(self.x, self.y, radius=0.1, kind='outside')
         v0 = 0.001*(np.sin(self.x)*np.sin(self.y)).flatten()
@@ -175,16 +179,15 @@ class PenaltyDE_2d:
         full_x0 = np.zeros((2*self.N**2,), dtype=complex)
         full_x0[:self.N**2] = v0.flatten()
         full_x0[self.N**2:] = w0.flatten()
-        self.x0 = full_x0
-        # Make sure that BC is satisfied
-        self.x0[self.bndry_points] = 0.
+        # Make sure that the value or derivative constraint is satisfied for
+        # both u and w without unnecessarily zeroing Neumann boundary pairs.
+        self.x0 = self.projc.project_feasible(full_x0)
     ''' -------------------------------------------------- '''
     # def time_evolution(self,  ode_method=RK45):
     def time_evolution(self,  ode_method=RK23):
         '''
             Function that simulates the time evolution using penalty constraints of the
             system operator defined in the class with respect to the initial conditions x0
-            Does not work for the wave equation with the dilation in [u]->[v;w]
             Returns:
             - val_array, which has been corrected by homogenization shift
             - bndry_val_array, to compute error with (not corrected by shift,
@@ -196,7 +199,10 @@ class PenaltyDE_2d:
         else:
             ipic = True
             if ipic:
-                RHS = lambda t: self.projc.apply_exp_fn(self.lam)(t) @ ( self.RHS + self.L_at_bcvals )
+                rhs_vals = self.RHS + self.L_at_bcvals
+                RHS = lambda t: self.projc.apply_exp_to(
+                    rhs_vals, self.lam, t
+                )
                 sol = ode_method(fun=ode_fn(operator=ipic_operator(self.operator, self.projc, self.lam),
                                             RHS=RHS), t0=0, y0=self.x0, t_bound=self.T,
                                             first_step=self.dt,
@@ -207,7 +213,6 @@ class PenaltyDE_2d:
                 sol = ode_method(fun=ode_fn(lambda t: self.operator - 1.j*self.lam*self.projc.operator, lambda t: self.RHS),
                         t0=0, y0=self.x0, t_bound=self.T, first_step=self.dt, max_step=self.dt)
                 '''
-
             n_steps = int(self.T*(1/self.dt))
             # t_array = np.zeros(n_steps)
             '''
@@ -218,15 +223,16 @@ class PenaltyDE_2d:
             val_array = np.zeros((2,self.N,self.N), dtype=complex)
             bndry_val_array = np.zeros((2,self.N**2), dtype=complex)
             val_array[0,:,:] = np.reshape(self.x0, (self.N,self.N))
-            bndry_val_array[0,self.bndry_points] =  self.x0[self.bndry_points]
-
+            bndry_val_array[0,:] = self.projc.constraint_residual(self.x0)
             ''' wanna change this to the scipy.solve_ivp function '''
             for i in range(1, n_steps):
                 sol.step()
                 # t_array[i] = sol.t
                 # convert back from interaction picture
                 if ipic:
-                    vals = self.projc.apply_exp_fn(self.lam)(-sol.t)@sol.y
+                    vals = self.projc.apply_exp_to(
+                        sol.y, self.lam, -sol.t
+                    )
                 elif not ipic:
                     vals = sol.y
                 '''
@@ -234,22 +240,24 @@ class PenaltyDE_2d:
                 bndry_val_array[i, self.bndry_points] = copy.deepcopy(vals[self.bndry_points])
                 '''
                 val_array[-1,:,:] = copy.deepcopy(np.reshape(vals, (self.N,self.N)))
-                bndry_val_array[-1, self.bndry_points] = copy.deepcopy(vals[self.bndry_points])
+                bndry_val_array[-1,:] = copy.deepcopy(
+                    self.projc.constraint_residual(vals)
+                )
                 # if not i%1000:
                 #     print('at step', i)
-
             val_array += self.bcvals.reshape((self.N,self.N))
 
             print('Finished after ', n_steps, ' steps.')
-
             return val_array, bndry_val_array
     ''' -------------------------------------------------- '''
     def _time_evolution_wave(self, ode_method=RK45):
         ''' time evolve the wave equation by variable substitution '''
-        # wave equation with source term not implemented yet
         ipic = True
         if ipic:
-            RHS = lambda t: self.projc.apply_exp_fn(self.lam)(t) @ ( self.RHS + self.L_at_bcvals )
+            rhs_vals = self.RHS + self.L_at_bcvals
+            RHS = lambda t: self.projc.apply_exp_to(
+                rhs_vals, self.lam, t
+            )
             sol = ode_method(fun=ode_fn(ipic_operator(self.operator, self.projc, self.lam),
                                         RHS), t0=0, y0=self.x0, t_bound=self.T,
                                                 first_step=self.dt,
@@ -259,7 +267,6 @@ class PenaltyDE_2d:
             RHS = self.RHS + self.L_at_bcvals
             sol = ode_method(fun=ode_fn(lambda t: self.operator - 1.j*self.lam*self.projc, RHS),
                              t0=0, y0=self.x0, t_bound=self.T, first_step=self.dt)
-
         n_steps = int(self.T*(1/self.dt))
         t_array = np.zeros(n_steps)
         '''
@@ -270,22 +277,21 @@ class PenaltyDE_2d:
         soln_bndry = np.zeros((2,2,self.N**2), dtype=complex)
         val_array[0,0,...] = np.reshape(self.x0[:self.N**2], (self.N,self.N))
         val_array[0,1,...] = np.reshape(self.x0[self.N**2:], (self.N,self.N))
-        # we assume that at initial time compliant to bndry conditions so having zero
-        # there is fine.
-        # I know this deviates from what's implemented in the case above but effectively
-        # are just doing the same at the moment.
-
+        initial_residual = self.projc.constraint_residual(self.x0)
+        soln_bndry[0,0,...] = initial_residual[:self.N**2]
+        soln_bndry[0,1,...] = initial_residual[self.N**2:]
         for i in range(n_steps):
             sol.step()
             t_array[i] = sol.t
             # convert back from interaction picture
             if ipic:
-                vals = self.projc.apply_exp_fn(self.lam)(-sol.t)@sol.y
+                vals = self.projc.apply_exp_to(
+                    sol.y, self.lam, -sol.t
+                )
             elif not ipic:
                 raise Exception("This needs to be implemented.")
                 # vals = ipic_rotation(self.projc, self.lam)(sol.t)@sol.y
-            bvals = np.zeros_like(vals)
-            bvals[self.bndry_points] = vals[self.bndry_points]
+            bvals = self.projc.constraint_residual(vals)
             '''
             val_array[ i,0,...] = np.reshape(vals[:self.N**2], (self.N,self.N))  # [v]
             val_array[ i,1,...] = np.reshape(vals[self.N**2:], (self.N,self.N))  # [w]
@@ -299,7 +305,6 @@ class PenaltyDE_2d:
             # if not i%100:
             #     print('at step', i)
         print('Finished after ', n_steps, ' steps.')
-
         return val_array, soln_bndry
     ''' -------------------------------------------------- '''
     def set_RHS(self, in_vals):
@@ -308,6 +313,11 @@ class PenaltyDE_2d:
     ''' -------------------------------------------------- '''
     def set_bcvals(self, in_vals):
         ''' set boundary values and associated additional source terms '''
-        self.bcvals = in_vals.flatten()
+        new_bcvals = in_vals.flatten()
+        if self.projc.kind == 'deriv' and np.any(new_bcvals):
+            raise NotImplementedError(
+                'Only homogeneous (zero) Neumann data is implemented.'
+            )
+        self.bcvals = new_bcvals
         print('Setting additional source terms according to time-independent BC.')
         self.L_at_bcvals = self.operator @ self.bcvals
